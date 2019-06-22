acf55307b3d45fef6a28c6da35bdfd5cdfc7de31
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-06-21 19:59:36 -0500

    added wins, losses, guess to index

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,21 @@
 
-console.log(5 + 6);
+// console.log(5 + 6);
 
 
 
 
-document.write("Wins:<br/>");
-document.write("Losses:<br/>");
-document.write("Guesses Left:<br/>");
-document.write("Your Guesses so far:<br/>");
+// document.write("Wins:<br/>");
+// document.write("Losses:<br/>");
+// document.write("Guesses Left:<br/>");
+// document.write("Your Guesses so far:<br/>");
+
+// how the computer will pick a letter
+// var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+// 
+
+
+// 
+// for (i=0; i<9; i++) {
 
